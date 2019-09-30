2d88532a9750be3763b2a25d4b484a5f9b1b6fa2
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-01 01:16:31 +0700

    menambahkan objeck type

--- Produk.php
+++ Produk.php
@@ -18,16 +18,14 @@ class Produk{
 	}
 
 }
+class CetakInfoProduk{
+	public function cetak( Produk $Produk ){
+		$str = "{$Produk->judul} | {$Produk->getLabel()}(Rp.{$Produk->harga})"; 
+		return $str;
+	}
+}
 
-// $Produk1 = new Produk();
-// $Produk1->judul= " naruto";
-// var_dump($Produk1);
-
-// $Produk2 = new Produk();
-// $Produk2->judul= " ucharted";
-// $Produk2->TambahProperti ="hahahaha";
-// var_dump($Produk2);
-$Produk3 = new Produk("naruto","masashi kishimoto","masashi kishimoto","jump",30000);
+$Produk3 = new Produk("naruto","masashi kishimoto","jump",30000);
 
 $Produk4 = new Produk("uncharted","neil druckman","sony computer",2530000);
 
@@ -35,5 +33,7 @@ $Produk4 = new Produk("uncharted","neil druckman","sony computer",2530000);
 echo "komik : " .$Produk3->getLabel();
 echo "<br>";
 echo "game : " .$Produk4->getLabel();
-
+echo "<br>";
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($Produk3); 
 
