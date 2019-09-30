7ae33b4bfab81624f0c22eba3d4a1c9b2a9db3e7
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-01 04:31:14 +0700

    mencoma menambahkan namespace

---
+++ namespace/App/Produk/User.php
@@ -0,0 +1,6 @@
+<?php namespace App\Produk;
+class User {
+	public function __construct(){
+		echo "ini adalah classs ". __CLASS__;
+	}
+}
---
+++ namespace/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once 'App/init.php';
+
+// $Produk3 = new komik("naruto","masashi kishimoto","jump",30000 , 100);
+
+// $Produk4 = new game("uncharted","neil druckman","sony computer",2530000 ,50);
+
+// $CetakProduk =  new CetakInfoProduk();
+// $CetakProduk->tambahProduk($Produk3);
+// $CetakProduk->tambahProduk($Produk4);
+// echo $CetakProduk->cetak();
+// echo "<hr>";
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
