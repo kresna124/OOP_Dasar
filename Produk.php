c829d270968fe95eadb982e56b333fcd88bdcd96
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-01 02:46:43 +0700

    menambahkan seter dan geter

--- Produk.php
+++ Produk.php
@@ -1,11 +1,12 @@
 <?php 
 
 class Produk{
-	public $judul,
-		   $penulis,
-		   $penerbit;
-	protected $diskon = 0;
-	private $harga;
+	private $judul,
+		    $penulis,
+		    $penerbit,
+		    $harga,
+		    $diskon = 0;
+	
 
 	// ===================================================================	   
 	public function __construct($judul ="judul" , $penulis = "penulis" , $penerbit = "penerbit", $harga = 0){
@@ -15,10 +16,40 @@ class Produk{
 		$this->harga = $harga;
 	}
 	// ===================================================================
+	public function setJudul($judul){
+		$this->judul=$judul;
+	}
+	public function getJudul(){
+		return $this->judul;
+	}
+	// ===================================================================
+	public function setPenulis($penulis){
+		$this->penulis=$penulis;
+	}
+	public function getPenulis(){
+		return $this->penulis;
+	}
+	// ===================================================================
+	public function setPenerbit($penerbit){
+		$this->penerbit=$penerbit;
+	}
+	public function getPenerbit(){
+		return $this->penerbit;
+	}
+	// ===================================================================
+	public function setDiskon($diskon){
+		$this->diskon = $diskon;
+	}
+	public function getDiskon(){
+		return $this->diskon;
+	}
+	// ===================================================================
+	public function setHarga($harga){
+		$this->harga=$harga;
+	}
 	public function getHarga(){
 		return $this->harga- ($this->harga * $this->diskon / 100);
 	}
-	
 	// ===================================================================
 	public function getLabel(){
 		return "$this->penulis,$this->penerbit";
@@ -62,10 +93,7 @@ class game extends Produk{
 		$str = "game :". parent::getInfoProduk()."- {$this->waktuMain} jam.";
 		return $str;
 	}
-	// ===================================================================
-	public function setDiskon($diskon){
-		$this->diskon = $diskon;
-	}
+	
 }
 
 // ===================================================================
@@ -95,3 +123,6 @@ echo "<hr>";
 
 $Produk4->setDiskon(50);
 echo $Produk4->getHarga();
+echo "<hr>";
+
+echo $Produk3->getJudul();
